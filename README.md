0e0dcd8020db04eca4331f0f3f0bcb7092945f3d
FosterQU26 <21fje@queensu.ca>
2025-01-15 16:29:23 -0500

    Inital Testing Script

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # ELEC-390-Group-21
+
+GitHub Repository for Group 21 in ELEC 390
+Authors: Foster Ecklund, Ben Wyand, Liam Etty, Gopeshan Mathan...
---
+++ Testing.py
@@ -0,0 +1,43 @@
+#Testing Scripts in Python for the project
+
+from picarx import Picarx
+import time
+
+if __name__ == "__main__" :
+    try:
+        #initilize a Picarx object
+        px = Picarx()
+
+        #Motor Test
+        px.forward(10)      #Move forward
+        """time.sleep(0.5)
+        px.backward(10)     #Move Backward
+        time.sleep(0.5)
+        #Test Cervos
+        for angle in range(0, 35):
+            px.set_dir_servo_angle(angle)
+            px.set_cam_pan_angle(angle)
+            time.sleep(0.01)
+        for angle in range(35, -35, -1):
+            px.set_dir_servo_angle(angle)
+            px.set_cam_pan_angle(angle)
+            time.sleep(0.01)
+        for angle in range(-35, 0):
+            px.set_dir_servo_angle(angle)
+            px.set_cam_pan_angle(angle)
+            time.sleep(0.01)
+        
+        #Turn Camera up Down
+        for angle in range(0, 35):
+            px.set_cam_tilt_angle(angle)
+            time.sleep(0.01)
+        for angle in range(35, -35,-1):
+            px.set_cam_tilt_angle(angle)
+            time.sleep(0.01)        
+        for angle in range(-35, 0):
+            px.set_cam_tilt_angle(angle)
+            time.sleep(0.01)"""
+
+    finally:
+        px.stop()
+        time.sleep(0.2)    
